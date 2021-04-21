9550ab9e066eee7a5126c7f2888407636f74bd87
Gunmar279 <72554970+Gunmar279@users.noreply.github.com>
2021-04-21 21:15:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload() {
+
+}
+  
+  function setup() {
+   canvas = createCanvas(400, 400);
+   canvas.position(500,100)
+   video = createCapture(VIDEO)
+   video.hide();
+   tint_color = "";
+  }
+  
+  function draw() {
+    image(video, 0, 0, 400, 400);
+    tint(tint_color)
+  }
+
+  function take_snapshot(){
+      save("filter_image")
+  }
